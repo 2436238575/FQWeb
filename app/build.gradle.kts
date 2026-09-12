a6eec2ae03b3dcb597da128776d5f449812a68bd
熵 <2436238575@qq.com>
2026-09-13 04:36:23 +0800

    chore: 版本号重写为 0.2.3 省电修改按修订号计

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "me.fycz.FQWeb"
         minSdk = 24
         targetSdk = 33
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
