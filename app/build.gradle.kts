91965154d55142d107a1d2cc3a9e2062f201f1b9
熵 <2436238575@qq.com>
2026-09-13 04:53:15 +0800

    chore: 版本号更新到 0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "me.fycz.FQWeb"
         minSdk = 24
         targetSdk = 33
-        versionCode = 5
-        versionName = "0.2.3"
+        versionCode = 6
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
