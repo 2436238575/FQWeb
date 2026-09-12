e2d003784c5434d7811ca42b2de4b223293e5389
熵 <2436238575@qq.com>
2026-09-13 02:33:08 +0800

    fix: 修正 findMethodOrNull 缺失方法时抛异常而非返回 null

--- app/src/main/java/me/fycz/fqweb/utils/KotlinXposedHelper.kt
+++ app/src/main/java/me/fycz/fqweb/utils/KotlinXposedHelper.kt
@@ -393,8 +393,8 @@ fun String.findClassOrNull(classLoader: ClassLoader?): Class<*>? =
 fun String.findMethod(classLoader: ClassLoader?, method: String?, vararg args: Any?): Method =
     XposedHelpers.findMethodExact(this, classLoader, method, args)
 
-fun String.findMethodOrNull(classLoader: ClassLoader?, method: String?, vararg args: Any?): Method =
-    XposedHelpers.findMethodExact(this, classLoader, method, args)
+fun String.findMethodOrNull(classLoader: ClassLoader?, method: String?, vararg args: Any?): Method? =
+    XposedHelpers.findMethodExactIfExists(this, classLoader, method, *args)
 
 fun Class<*>.new(vararg args: Any?): Any = XposedHelpers.newInstance(this, *args)
 
